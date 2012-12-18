30fc82e5acbacd504b8f53d80150937adfa6198d
tyrel86 <tyrelrichey@gmail.com>
2012-12-17 18:23:53 -0700

    First Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Sdodonnell::Application.config.secret_token = '4673a1eb8cba107a447f53d4eea3f91c7be66a358d23559a619d5c2f12ee58043b305a82fe722d9cd38141a24711de575705b24af8085b332c9b10db0f3b35f5'
