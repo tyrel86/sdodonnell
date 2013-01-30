600f741ef8b7435aa374249e13d6675f9dcfae03
tyrel86 <tyrelrichey@gmail.com>
2013-01-29 17:28:31 -0700

    Done and done

--- app/mailers/main_mailer.rb
+++ app/mailers/main_mailer.rb
@@ -5,11 +5,11 @@ class MainMailer < ActionMailer::Base
   end
 	def confirm_signup_email( subscriber )
 		@subscriber = subscriber
-    mail(:to => subscriber.email, :subject => "Confirmation of S.D. O'donnell's Mailing list")
+    mail(:to => subscriber.email, :subject => "Confirmation of S.D. O'Donnell's Mailing list")
   end
 	def mailing_list_email( subscriber )
 		@subscriber = subscriber
-    mail(:to => subscriber.email, :subject => "News from S.D. O'donnell's")
+    mail(:to => subscriber.email, :subject => "News from S.D. O'Donnell's")
   end
 	def contact_form_email( contact )
 		@contact = contact
